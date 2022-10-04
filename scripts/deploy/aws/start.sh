73b6607b7e1083d9a6008568d3d9ac274061a7d2
kysee <kysee@hanmail.net>
2022-10-04 16:04:52 +0900

    style: Update deploy scripts

--- scripts/deploy/aws/start.sh
+++ scripts/deploy/aws/start.sh
@@ -2,22 +2,31 @@
 
 ARCANUS_DATADIR="$HOME/.arcanus"
 ARCANUS_LOG="${ARCANUS_DATADIR}/log"
-ARCANUS_BIN="$HOME/bin/arcanus"
+ARCANUS_BINFILE="$HOME/arcanus-deploy/arcanus"
+ARCANUS_INITDIR="$HOME/arcanus-deploy/.arcanus"
+
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "arcanus-dev-init-dg" ]; then
-
+  #
+  # Remove ~/.arcanus and copy ~//arcanus-deploy/.arcanus to ~/.arcanus
+  #
   if [[ -d "$ARCANUS_DATADIR" ]]; then
     echo "Remove ${ARCANUS_DATADIR} ..."
     rm -rf ${ARCANUS_DATADIR}
   fi
 
-  echo "Initialize ${ARCANUS_DATADIR} ..."
-  ${ARCANUS_BIN} init --chain_id demonet --priv_validator_secret '1' # 1>${ARCANUS_LOG} 2>&1
+  echo "Copy '${ARCANUS_INITFILES}' to '${ARCANUS_DATADIR}' ..."
+
+  # copy ./.arcanus to ~/.arcanus
+  cp -rf ${ARCANUS_INITFILES} ${ARCANUS_DATADIR}
 
 elif [ "$DEPLOYMENT_GROUP_NAME" == "arcanus-dev-reset-dg" ]; then
+  #
+  # Reset ~/.arcanus. Remove blockchain data.
+  #
   echo "Reset data in ${ARCANUS_DATADIR}..."
-  ${ARCANUS_BIN} unsafe-reset-all --priv_validator_secret '1' # 1>${ARCANUS_LOG} 2>&1
+  ${ARCANUS_BINFILE} unsafe-reset-all --priv_validator_secret '1' # 1>${ARCANUS_LOG} 2>&1
 fi
 
 echo "Start arcanus ..."
-nohup ${ARCANUS_BIN} start --rpc.laddr 'tcp://0.0.0.0:26657' --rpc.cors_allowed_origins '*' --priv_validator_secret '1' 1>${ARCANUS_LOG} 2>&1 &
+nohup ${ARCANUS_BINFILE} start --rpc.laddr 'tcp://0.0.0.0:26657' --rpc.cors_allowed_origins '*' --priv_validator_secret '1' 1>${ARCANUS_LOG} 2>&1 &
