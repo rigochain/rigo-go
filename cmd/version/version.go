862c589716d9610d8fc3b7cca82b173a733566b6
kylekwon <kyle.kwon@themedium.io>
2023-09-19 21:56:14 +0900

    feat: Upgrade to Tendermint v0.34.24

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -12,8 +12,8 @@ const (
 
 var (
 	majorVer  uint64 = 1
-	minorVer  uint64 = 0
-	patchVer  uint64 = 9
+	minorVer  uint64 = 1
+	patchVer  uint64 = 0
 	commitVer uint64 = 0
 
 	// it is changed using ldflags.
