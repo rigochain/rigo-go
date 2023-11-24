41281d9ec678d088d3dd79c3024d2c21abb72b70
maurice.kang <maurice.kang@rigolabs.io>
2023-11-24 18:57:04 +0900

    validate_svc.sh 서비스 검증 로직 버그 수정

--- scripts/codedeploy/validate_svc.sh
+++ scripts/codedeploy/validate_svc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 10
+
 # set service var
 service=$(hostname | cut -d "-" -f3 | tr -cd '[:alpha:]')
 
@@ -7,26 +9,35 @@ echo "$service"
 
 case $service in
   validator*)
-    rs=$(curl -f 127.0.0.1:26657/abci_info)
-    if [ "$rs" -eq 0 ]; then
-      echo "$rs test success"
+    curl -f 127.0.0.1:26657/abci_info
+    if [ $? == 0 ]; then
+      echo "validate test successed"
+      echo "return value is $?"
     else
+      echo "validate test failed"
+      echo "return value is $?"
       exit 1
     fi
     ;;
   sentry)
-    rs=$(curl -f 127.0.0.1:26657/abci_info)
-    if [ "$rs" -eq 0 ]; then
-      echo "$rs test success"
+    curl -f 127.0.0.1:26657/abci_info
+    if [ $? == 0 ]; then
+      echo "validate test successed"
+      echo "return value is $?"
     else
+      echo "validate test failed"
+      echo "return value is $?"
       exit 1
     fi
     ;;
   rpc)
-    rs=$(curl -f 127.0.0.1:26657/abci_info)
-    if [ "$rs" -eq 0 ]; then
-      echo "$rs test success"
+    curl -f 127.0.0.1:26657/abci_info
+    if [ $? == 0 ]; then
+      echo "validate test successed"
+      echo "return value is $?"
     else
+      echo "validate test failed"
+      echo "return value is $?"
       exit 1
     fi
     ;;
